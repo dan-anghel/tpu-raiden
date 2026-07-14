82c085bdbd6eb25cfa8516e80967faa12f6fd16b
justinlu <justinlu@google.com>
2026-07-14 13:23:15 -0700

    Add ControllerServer singleton gRPC server wrapper.
    PiperOrigin-RevId: 947869734

---
+++ tpu_raiden/core/controller/controller_server.cc
@@ -0,0 +1,88 @@
+// Copyright 2026 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tpu_raiden/core/controller/controller_server.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+#include "absl/synchronization/mutex.h"
+#include "grpcpp/security/server_credentials.h"
+#include "grpcpp/server_builder.h"
+#include "tpu_raiden/core/controller/controller_service.h"
+
+namespace tpu_raiden {
+namespace core {
+namespace controller {
+
+ControllerServer& ControllerServer::GetInstance() {
+  static ControllerServer* instance = new ControllerServer();
+  return *instance;
+}
+
+absl::Status ControllerServer::StartServer(int port) {
+  if (port < 0) {
+    return absl::InvalidArgumentError(
+        absl::StrCat("Invalid gRPC port: ", port));
+  }
+
+  absl::MutexLock lock(mutex_);
+  if (started_) {
+    if (port != 0 && port != grpc_port_) {
+      return absl::FailedPreconditionError(
+          absl::StrCat("Server already started on port ", grpc_port_,
+                       ", cannot start on requested port ", port));
+    }
+    return absl::OkStatus();
+  }
+
+  controller_service_ = std::make_unique<RaidenControllerServiceImpl>();
+
+  std::string server_address = absl::StrCat("[::]:", port);
+  grpc::ServerBuilder builder;
+  int selected_port = 0;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials(),
+                           &selected_port);
+  builder.RegisterService(controller_service_.get());
+  grpc_server_ = builder.BuildAndStart();
+
+  if (!grpc_server_ || selected_port == 0) {
+    grpc_server_.reset();
+    controller_service_.reset();
+    return absl::InternalError(
+        absl::StrCat("Failed to start gRPC server on port: ", port, " (",
+                     server_address, ")"));
+  }
+
+  grpc_port_ = selected_port;
+  started_ = true;
+  return absl::OkStatus();
+}
+
+int ControllerServer::GetGrpcPort() const {
+  absl::MutexLock lock(mutex_);
+  return grpc_port_;
+}
+
+RaidenControllerServiceImpl* ControllerServer::GetControllerService() const {
+  absl::MutexLock lock(mutex_);
+  return controller_service_.get();
+}
+
+}  // namespace controller
+}  // namespace core
+}  // namespace tpu_raiden
---
+++ tpu_raiden/core/controller/controller_server_test.cc
@@ -0,0 +1,86 @@
+// Copyright 2026 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tpu_raiden/core/controller/controller_server.h"
+
+#include <memory>
+#include <string>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/status/status.h"
+#include "absl/status/status_matchers.h"
+#include "tpu_raiden/core/controller/controller_client.h"
+
+namespace tpu_raiden {
+namespace core {
+namespace controller {
+namespace {
+
+using ::absl_testing::StatusIs;
+
+TEST(ControllerServerTest, StartServerAndGetPortWorks) {
+  ControllerServer& server = ControllerServer::GetInstance();
+  ABSL_ASSERT_OK(server.StartServer(/*port=*/0));
+  int port = server.GetGrpcPort();
+  EXPECT_GT(port, 0);
+
+  EXPECT_NE(server.GetControllerService(), nullptr);
+
+  // Connect via client and verify gRPC communication works.
+  std::string server_address = "localhost:" + std::to_string(port);
+  RaidenControllerClient client(server_address);
+
+  absl::Status status =
+      client.RegisterWorker("worker_0", "localhost:10001", "localhost:10002");
+  ABSL_ASSERT_OK(status);
+
+  auto worker_or = server.GetControllerService()->GetWorker("worker_0");
+  ABSL_ASSERT_OK(worker_or);
+  EXPECT_EQ(worker_or->worker_id, "worker_0");
+  EXPECT_EQ(worker_or->raiden_worker_endpoint, "localhost:10001");
+  EXPECT_EQ(worker_or->raiden_transfer_endpoint, "localhost:10002");
+}
+
+TEST(ControllerServerTest, SingletonIsReused) {
+  ControllerServer& server1 = ControllerServer::GetInstance();
+  ABSL_ASSERT_OK(server1.StartServer(/*port=*/0));
+  int port1 = server1.GetGrpcPort();
+  EXPECT_GT(port1, 0);
+
+  ControllerServer& server2 = ControllerServer::GetInstance();
+  ABSL_ASSERT_OK(server2.StartServer(/*port=*/0));
+  int port2 = server2.GetGrpcPort();
+  EXPECT_EQ(port1, port2);
+}
+
+TEST(ControllerServerTest, StartServerWithInvalidPortFails) {
+  ControllerServer& server = ControllerServer::GetInstance();
+  absl::Status status = server.StartServer(/*port=*/-1);
+  EXPECT_THAT(status, StatusIs(absl::StatusCode::kInvalidArgument));
+}
+
+TEST(ControllerServerTest, StartServerWithConflictingPortFails) {
+  ControllerServer& server = ControllerServer::GetInstance();
+  ABSL_ASSERT_OK(server.StartServer(/*port=*/0));
+  int active_port = server.GetGrpcPort();
+  int different_port = (active_port == 12345) ? 12346 : 12345;
+  absl::Status status = server.StartServer(different_port);
+  EXPECT_THAT(status, StatusIs(absl::StatusCode::kFailedPrecondition));
+}
+
+}  // namespace
+}  // namespace controller
+}  // namespace core
+}  // namespace tpu_raiden
